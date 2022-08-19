71de2f62ac43fa7b10b52c137c8e8cd138f9f3e3
iDevice02 <111559547+iDevice02@users.noreply.github.com>
2022-08-19 12:38:27 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+<html>
+  <head>
+    <title>
+      HI
+      </title>
+    </head>
+  <body>
+        HI 
+    
+    <form method="get" action="admin.php">
+    <button type="submit">Continue</button>
+</form>
+  </html>
+    </body>
